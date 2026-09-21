21a57472ccf04e93ccf1feef2fb01587d63f1da4
Dimamov <dmitriymovsesyan@gmail.com>
2026-09-20 22:31:34 -0400

    Refresh app cache for hero layout fix

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='hahn-v5';
+const CACHE='hahn-v6';
 const ASSETS=['./','./index.html','./ChatGPT Image Sep 20, 2026, 10_01_06 PM.png','./21BD3E27-9655-4D63-BDF7-15590C919AA6.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
