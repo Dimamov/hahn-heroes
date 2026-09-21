0d9e8c8ef071c494dcc3ca685ee787fdbf1286e1
Dimamov <dmitriymovsesyan@gmail.com>
2026-09-20 22:06:56 -0400

    Cache final H.A.H.N. artwork

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hahn-v2';
-const ASSETS=['/','/index.html','/hahn-concept.jpg','/manifest.webmanifest','/app-icon.svg'];
+const CACHE='hahn-v3';
+const ASSETS=['/','/index.html','/ChatGPT Image Sep 20, 2026, 10_01_06 PM.png','/manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))));
