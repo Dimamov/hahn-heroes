b54ccb84a7e7fccb1110b5e79a583d491c34dacc
Dimamov <dmitriymovsesyan@gmail.com>
2026-09-21 15:05:29 -0400

    Force refresh for Fun Box bug fix

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='hahn-v11';
+const CACHE='hahn-v12';
 const ASSETS=['./','./index.html','./ChatGPT Image Sep 20, 2026, 10_01_06 PM.png','./app-icon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
