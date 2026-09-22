a7eacdb50ce184c9c1583fed5710063496ed3b21
Dimamov <dmitriymovsesyan@gmail.com>
2026-09-21 20:05:30 -0400

    Refresh app cache for Nexus Word Rush

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hahn-v14';
-const ASSETS=['./','./index.html','./ChatGPT Image Sep 20, 2026, 10_01_06 PM.png','./app-icon.svg','./manifest.webmanifest'];
+const CACHE='hahn-v15';
+const ASSETS=['./','./index.html','./word-rush.js','./ChatGPT Image Sep 20, 2026, 10_01_06 PM.png','./app-icon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))));
